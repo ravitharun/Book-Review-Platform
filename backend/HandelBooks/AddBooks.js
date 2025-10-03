921332c6c1d6b532b0a82e8b322ad8b9e682fa69
Ravi Tharun <tr565003@gmail.com>
2025-10-03 22:09:34 +0530

    added the all requried routes in the sever

--- backend/HandelBooks/AddBooks.js
+++ backend/HandelBooks/AddBooks.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// these route Is used to get the Book Data
+router.get("/Book/Add", async (req, res) => {
+  try {
+    const { Tittle, Description, Genre, Author, PublishYear } = req.query
+    if (!Tittle || !Description || !Genre || !Author || !PublishYear) {
+      return res.json({ message: "Fill the required Detils in the form" })
+    }
+    // save into the DB
+
+  } catch (error) {
+    return res.statusCode(500).json({ message: error.message })
+  }
+})
+
+// In these route we will get the all books with review also
+router.get("Book/GetAllBooks", async (req, res) => {
+  try {
+    // In these route we will get the all booksn From the DB with review also and return statcode(200)==>{message:BooksAll}  
+
+  }
+  catch (err) {
+    return res.statusCode(500).json({ message: err.message })
+  }
+})
+
+// in these route we will delete the  Books based on Id
+router.put("/Books/UpdateBook/:Bookid", async (req, res) => {
+  try {
+    const { BookId } = req.params
+    if (!BookId) {
+      return res.statusCode(404).json({ message: "BookId is not Found.." })
+    }
+    // here we will adding the dbquery to delete the book based on the uinque if delete sucess return res.stcode(200)=>removed the book if not statuscode(404)===>'some thing went wrong'
+  } catch (error) {
+    return res.statusCode(500).json({ message: error.message })
+  }
+})
+
+
+
+
+
+// in these route we will delete the  Books based on Id
+router.delete("/Books/DeleteBook/:Bookid", async (req, res) => {
+  try {
+    const { BookId } = req.params
+    if (!BookId) {
+      return res.statusCode(404).json({ message: "BookId is not Found.." })
+    }
+    // here we will adding the dbquery to delete the book based on the uinque if delete sucess return res.stcode(200)=>removed the book if not statuscode(404)===>'some thing went wrong'
+  } catch (error) {
+    return res.statusCode(500).json({ message: error.message })
+  }
+})
+
 
 module.exports = router;
--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,15 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var AuthRouter = require('./Authentication/AuthLogin');
+var HandelBooksRouter = require('./HandelBooks/AddBooks');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/BookReview/Auth', AuthRouter);
+app.use('/BookReview/HandelRouter', HandelBooksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
