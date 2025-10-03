041970a41aa39870eae564860753873e3e246512
Ravi Tharun <tr565003@gmail.com>
2025-10-03 09:49:24 +0530

    updated the navabar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,15 +8,17 @@ import Books from './Components/Pages/Books.jsx';
 import BookDetails from './Components/Pages/BookDetails.jsx';
 import Login from './Components/Auth/Login.jsx';
 import Signup from './Components/Auth/Signup.jsx';
+import Profile from './Components/Pages/Profile.jsx';
 createRoot(document.getElementById('root')).render(
   // <Routes>
           <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/books" element={<Books />} />
-        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/BooksDetails" element={<BookDetails />} />
       </Routes>
     </Router>
 );
